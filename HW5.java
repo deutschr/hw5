74377910180f9d4673c9eaa13daea77a39ed86cb
RonPC\Ron <rmd97@aol.com>
2016-11-13 21:33:34 -0500

    added comments and printing the array before sorting

--- HW5.java
+++ HW5.java
@@ -1,8 +1,13 @@
 public class BubbleSort
 {
   public static void main(String[] args) {
+    //array of numbers to be sorted
     int array[] = new int[]{10,35,67,420,19,1789};
     
+    //print the array before sorting
+    for(int i=0; i<array.length;i++){
+      System.out.println(array[i] + " ");
+    }
     
   }
   
